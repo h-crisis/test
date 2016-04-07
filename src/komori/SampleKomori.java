295a4ba9591990d65abf397c24f9c3266e8c4a7d
KKomor <kenichiro.komori@hcrisis2.onmicrosoft.com>
2016-04-07 15:50:23 +0900

    First Push by Kenichiro Komori

---
+++ src/komori/SampleKomori.java
@@ -0,0 +1,10 @@
+package komori;
+
+/**
+ * Created by komori on 2016/04/07.
+ */
+public class SampleKomori {
+    public static void main(String args[]){
+        System.out.println("Hello!");
+    }
+}
